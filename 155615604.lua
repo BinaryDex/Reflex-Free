f357b9b7cb70bdf0f0c326d44977b1c914ec2002
BinaryDex <82985536+BinaryDex@users.noreply.github.com>
2022-11-26 12:14:28 -0500

    Update 155615604.lua

--- 155615604.lua
+++ 155615604.lua
@@ -1,1 +1,12 @@
-print('prison life here')
+if getgenv().qpidhiqwqq == true and _G.hi == false and test == "balls" and reflexfree == "accepted?" then
+    print("passed")
+    getgenv().passed = true;
+else
+    warn("failed")
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/BinaryDex/Reflex-Free/main/ReflexLoader.lua", true))()
+    wait(5)
+end
+
+if getgenv().passed then
+wait(1.5)
+end
