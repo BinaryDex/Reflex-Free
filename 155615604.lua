ccbf2d389545603aa25d846b6d6707604dbd5664
BinaryDex <82985536+BinaryDex@users.noreply.github.com>
2022-11-29 16:28:23 -0500

    Update 155615604.lua

--- 155615604.lua
+++ 155615604.lua
@@ -1,12 +1,12 @@
 if getgenv().qpidhiqwqq == true and _G.hi == false and test == "balls" and reflexfree == "accepted?" then
     print("passed")
-    getgenv().passed = true;
+    
+print('some code here!')    
+    
+    
+    
 else
     warn("failed")
     loadstring(game:HttpGet("https://raw.githubusercontent.com/BinaryDex/Reflex-Free/main/ReflexLoader.lua", true))()
     wait(5)
-end
-
-if getgenv().passed then
-wait(1.5)
 end
